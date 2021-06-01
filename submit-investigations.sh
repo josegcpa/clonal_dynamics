214055d48afbd693da78d6f61af32e68f219d426
josegcpa <josegcpa@ebi.ac.uk>
2021-06-01 14:32:08 +0100

    updating with notebooks with Emily's data

--- submit-investigations.sh
+++
@@ -1,5 +0,0 @@
-bsub -n 4 -M 16000 Rscript Scripts/investigate_simulations_competition.R
-
-bsub -n 4 -M 16000 Rscript Scripts/investigate_simulations_developmental.R
-
-bsub -n 4 -M 16000 Rscript Scripts/investigate_simulations_early_late.R
--- submit_simmulations.sh
+++
@@ -1,3 +0,0 @@
-bsub -n 4 -M 8000 -o /dev/null -e /dev/null Rscript Scripts/run_simulation_50k_1.R
-bsub -n 4 -M 8000 -o /dev/null -e /dev/null Rscript Scripts/run_simulation_100k_5.R
-bsub -n 4 -M 8000 -o /dev/null -e /dev/null Rscript Scripts/run_simulation_200k_13.R
